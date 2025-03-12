e4583e75b0c930722c885f131ddc5692970c341c
Stepan Rudenskyi <s.rudenskiy@gmail.com>
2025-03-12 16:23:30 +0100

    add httpBasic

--- src/main/java/org/example/petstore/config/WebSecurityConfig.java
+++ src/main/java/org/example/petstore/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -42,6 +43,7 @@ public class WebSecurityConfig {
                     registry.requestMatchers("/admin/**").hasRole("ADMIN");
                     registry.anyRequest().authenticated();
                 })
+                .httpBasic(Customizer.withDefaults())
                 .formLogin(form -> {
                     form.loginPage("/login");
                     form.successHandler(new CustomAuthenticationSuccessHandler());
