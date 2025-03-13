8abaf8c4128cf8d632156889671fb0cdde99487b
Stepan Rudenskyi <s.rudenskiy@gmail.com>
2025-03-13 15:29:39 +0100

    service and controller for departments

---
+++ src/main/java/org/example/petstore/controller/api/DepartmentController.java
@@ -0,0 +1,30 @@
+package org.example.petstore.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import org.example.petstore.dto.DepartmentDto;
+import org.example.petstore.service.DepartmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    /**
+     * Retrieves a list of all product departments.
+     *
+     * @return a ResponseEntity containing a list of DepartmentDto objects representing the categories
+     */
+    @GetMapping()
+    public ResponseEntity<List<DepartmentDto>> getCategories() {
+        List<DepartmentDto> departments = departmentService.findAllDepartments();
+        return ResponseEntity.ok(departments);
+    }
+}
---
+++ src/main/java/org/example/petstore/service/DepartmentService.java
@@ -0,0 +1,26 @@
+package org.example.petstore.service;
+
+import org.example.petstore.dto.DepartmentDto;
+import org.example.petstore.mapper.DepartmentMapper;
+import org.example.petstore.repository.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+    private final DepartmentMapper departmentMapper;
+
+    public DepartmentService(DepartmentRepository departmentRepository, DepartmentMapper departmentMapper) {
+        this.departmentRepository = departmentRepository;
+        this.departmentMapper = departmentMapper;
+    }
+
+    public List<DepartmentDto> findAllDepartments() {
+        return departmentRepository.findAll().stream()
+                .map(departmentMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
