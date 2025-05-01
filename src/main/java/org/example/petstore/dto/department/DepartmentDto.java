9a25bb9e4c99704544cbab30ce70860d1f325610
Stepan Rudenskyi <s.rudenskiy@gmail.com>
2025-05-01 11:33:54 +0200

    changed dto names for clarity

--- src/main/java/org/example/petstore/dto/department/DepartmentDto.java
+++ src/main/java/org/example/petstore/dto/department/DepartmentDto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class DepartmentDto {
-    private Long departmentId;
+    private Long id;
     private String name;
-    private String imagePath;
+    private String imageUrl;
 }
--- src/main/java/org/example/petstore/mapper/DepartmentMapper.java
+++ src/main/java/org/example/petstore/mapper/DepartmentMapper.java
@@ -3,9 +3,12 @@ package org.example.petstore.mapper;
 import org.example.petstore.dto.department.DepartmentDto;
 import org.example.petstore.model.Department;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
     Department toEntity(DepartmentDto departmentDto);
+    @Mapping(source = "department.departmentId", target = "id")
+    @Mapping(source = "department.imagePath", target = "imageUrl")
     DepartmentDto toDto(Department department);
 }
