6663342a18871a4fffb08a49cf22838069d7419b
Stepan Rudenskyi <s.rudenskiy@gmail.com>
2025-07-10 17:58:51 +0200

    security config

--- src/main/java/org/example/petstore/config/WebSecurityConfig.java
+++ src/main/java/org/example/petstore/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * and password encoding mechanisms.
  */
 @Configuration
-@Profile("!dev")
+@Profile({"!test & !dev"})
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
---
+++ src/main/java/org/example/petstore/config/test/TestSecurityConfig.java
@@ -0,0 +1,30 @@
+package org.example.petstore.config.test;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Profile("test")
+@Slf4j
+public class TestSecurityConfig {
+
+    @PostConstruct
+    public void init() {
+        log.info("Loaded TestSecurityConfig (test profile)");
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
